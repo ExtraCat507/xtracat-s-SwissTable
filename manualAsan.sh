9ae0aa71e84a72ab7d865a58648b3a98d8bb0312
extra_cat <levsimonov2006@gmail.com>
2026-04-24 01:54:01 +0300

    +diff test to CI

--- manualAsan.sh
+++ manualAsan.sh
@@ -3,3 +3,4 @@
 cmake -B build-asan -G "MinGW Makefiles"  -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=ON
 cmake --build build-asan
 ctest --test-dir build-asan --output-on-failure
+read
--- manualRelease.sh
+++ manualRelease.sh
@@ -3,3 +3,4 @@
 cmake -B build-release -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build build-release
 ctest --test-dir build-release --output-on-failure
+read
